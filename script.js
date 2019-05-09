296c5661f555d1f67adc1aea0ba8b01a62c65dcb
Beckford <abeckford@tre.state.ma.us>
2019-05-09 11:25:52 -0400

    Color Guessing Game Second Commit

--- script.js
+++ script.js
@@ -5,21 +5,26 @@
     //     but this is a good place to start).  Here are the values: 
     
     
-    var listOfColors = ["rgb(255, 0, 0)",
+    var listOfColors = [
+    "rgb(255, 0, 0)",
     "rgb(255, 255, 0)",
     "rgb(0, 255, 0)",
     "rgb(0, 255, 255)",
     "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"];
+    "rgb(255, 0, 255)"
+];
 
     // Now select all 6 colored divs from the HTML page (using a DOM selector starting with document...) 
     // and store them in a variable called squares or something similar.
 
-     let squares = document.getElementsByClassName('box');
+     let squares = document.querySelectorAll('.box');
 
     //  Loop through all six squares on your HTML page and assign the six colors in your array, to those squares.
     //  Hint: this will look something like this: squares[index].style.backgroundColor = colors[index])
-let secretColor =  listOfColors[3];
+     let secretColor = listOfColors[3];
+     let colorDisplay = document.getElementById("colorDisplay");
+
+     colorDisplay.textContent = secretColor;
 
 
 // Now display the RGB color specified in Step #13 in the middle of your h1.  You can use a span to do this. 
@@ -27,8 +32,17 @@ let secretColor =  listOfColors[3];
 // You can use something like: variable_name.innerHTML = secretColor;
 
 
-document.getElementById("secret").innerHTML = secretColor;
+
 
      for (var i = 0; i < squares.length; i++) {
-         squares[i].style.backgroundColor = listOfColors[i];
-     }
+         // add initial colors to sqaures
+        squares[i].style.backgroundColor = listOfColors[i];
+        squares[i].addEventListener("click", function(){
+            var clickedColor = this.style.backgroundColor;
+            if (clickedColor === secretColor) {
+                document.getElementById("newColors") = "Correct!";
+            } else {
+                this.style.backgroundColor = "rgb(35, 35, 35)";
+            }
+            });
+        }
